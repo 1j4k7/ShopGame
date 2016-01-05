be0e29097905ef9c86e5f3494af0e15399257519
thespacex101 <Spacew101>
2016-01-05 08:26:07 -0500

    Theoretical imports and basic render method

--- core/src/com/shop/game/ShopGame.java
+++ core/src/com/shop/game/ShopGame.java
@@ -34,16 +34,46 @@ public class ShopGame extends ApplicationAdapter {
 
 	@Override
 	public void create() {
-		
+		//81 complex items and 65 basic items
+		//basic items are 0-64 and complex items are 65-145
+		itemTextures = new Texture[146];
+		itemSprites = new Sprite[146];
+		for(int i=0;i<65;i++){ //imports the basic items
+			String name = "BI";
+			if(i<10)
+				name = name+"0"+(i+1)+".png";
+			else
+				name = name+(i+1)+".png";
+			itemTextures[i] = new Texture(Gdx.files.internal(name));
+			itemSprites[i] = new Sprite(itemTextures[i]);
+		}
+		for(int i=0;i<81;i++){ //imports the complex items
+			String name = "CI";
+			int index = i+65;
+			if(i<10)
+				name = name+"0"+(i+1)+".png";
+			else
+				name = name+(i+1)+".png";
+			itemTextures[index] = new Texture(Gdx.files.internal(name));
+			itemSprites[index] = new Sprite(itemTextures[index]);
+		}
 	}
 
 	@Override
 	public void dispose() {
-
+		for(int i=0;i<itemTextures.length;i++){
+			itemTextures[i].dispose();
+		}
+		batch.dispose();
 	}
 
 	@Override
 	public void render() {
+		Gdx.gl.glClearColor(0,0,0,1); //clears the background and set it black
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		batch.begin();
+		//draw everything here
+		batch.end();
 	}
 }
