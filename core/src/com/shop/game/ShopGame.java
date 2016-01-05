4ae1d09289f6499ad2d0be6561552ae451990e7d
thespacex101 <Spacew101>
2016-01-05 08:46:43 -0500

    Successfully imports all the images

--- core/src/com/shop/game/ShopGame.java
+++ core/src/com/shop/game/ShopGame.java
@@ -36,21 +36,22 @@ public class ShopGame extends ApplicationAdapter {
 	public void create() {
 		//81 complex items and 65 basic items
 		//basic items are 0-64 and complex items are 65-145
+		batch = new SpriteBatch();
 		itemTextures = new Texture[146];
 		itemSprites = new Sprite[146];
 		for(int i=0;i<65;i++){ //imports the basic items
-			String name = "BI";
+			String name = "Item-Images\\Basic-Items\\BI";
 			if(i<10)
-				name = name+"0"+(i+1)+".png";
+				name = name+"0"+i+".png";
 			else
-				name = name+(i+1)+".png";
+				name = name+i+".png";
 			itemTextures[i] = new Texture(Gdx.files.internal(name));
 			itemSprites[i] = new Sprite(itemTextures[i]);
 		}
 		for(int i=0;i<81;i++){ //imports the complex items
-			String name = "CI";
+			String name = "Item-Images\\Complex-Items\\CI";
 			int index = i+65;
-			if(i<10)
+			if(i<9)
 				name = name+"0"+(i+1)+".png";
 			else
 				name = name+(i+1)+".png";
