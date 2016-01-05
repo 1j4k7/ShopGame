e0fe2f3e5468a6f17f549c5109ea54ed165638b9
Aaron S. <thespacex101@gmail.com>
2016-01-04 23:32:23 -0500

    LibGDX base
    the basic project as created by the libGDX setup

---
+++ desktop/src/com/shop/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shop.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shop.game.ShopGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShopGame(), config);
+	}
+}
