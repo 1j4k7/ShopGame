d7ce33f69661024c8a8d47c52ca32e9f7c4d6a3c
Jason Kuo <jasonkuo147@gmail.com>
2016-01-09 00:14:28 -0500

    itemsDict now holds all items and each item's information including name, icon, description, and components. Items can be accessed using the item name as a string.

--- core/src/com/shop/game/Item.java
+++ core/src/com/shop/game/Item.java
@@ -1,40 +1,69 @@
 package com.shop.game;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import java.util.ArrayList;
+
 /**
  * Created by Aaron on 1/5/2016.
  */
 public class Item {
-    private String name;
-    private String description;
-    private Item[] components;
 
-    public Item(String name, String description, Item[] components) {
+    private String name;
+    private Sprite icon;
+    private String description;
+    private ArrayList<Item> components;
+
+    public Item(String name, Sprite icon, String description, ArrayList<Item> components) {
         this.name = name;
+        this.icon = icon;
         this.description = description;
         this.components = components;
+    }
+
+    public Item(String name, Sprite icon) {
+        this.name = name;
+        this.icon = icon;
+        this.description = "";
+        this.components = new ArrayList<Item>();
+
     }
 
     public String getName() {
         return name;
     }
 
+    public Sprite getIcon() { return icon; }
+
     public String getDescription() {
         return description;
     }
 
-    public Item[] getComponents() {
+    public ArrayList<Item> getComponents() {
         return components;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIcon(Sprite icon) { this.icon = icon; }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setComponents(ArrayList<Item> components) {
+        this.components = components;
     }
 
     /**
      * Checks whether <code>item</code> is a component of this <code>item</code>.
+     *
      * @param item the item being checked
      * @return true if <code>item</code> is a component of this <code>item</code>, false otherwise.
      */
-    public boolean contains(Item item){
-        if(java.util.Arrays.asList(components).indexOf(item) >= 0)
-            return true;
-        else
-            return false;
+    public boolean contains(Item item) {
+        return components.contains(item);
     }
 }
--- core/src/com/shop/game/ShopGame.java
+++ core/src/com/shop/game/ShopGame.java
@@ -4,39 +4,55 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ShopGame extends ApplicationAdapter {
 
+    //file reading
+    private FileHandle itemListHandle;
+    private FileHandle itemAssemblyHandle;
+    private FileHandle itemDescriptionHandle;
+
+    //general
     private SpriteBatch batch;
     private Texture[] itemTextures;
     private Sprite[] itemSprites;
+
+    //map of all items (key: name, value:Item)
+    private HashMap<String, Item> itemsDict;
+
+    //text
     private int guessesLeft;
     private BitmapFont guessesLeftText;
     private int score;
     private BitmapFont scoreText;
     private int consecutive;//number correct in a row
     private BitmapFont consecutiveText;
-    private ArrayList<Integer> recipes;
-    private HashMap<String, Integer> itemsDict;
 
-    private FileHandle itemListHandle;
-    private FileHandle itemAssemblyHandle;
+    //graphics
+    private ShapeRenderer shapeRenderer;
+    private Rectangle mainItemBox;
+    private ArrayList<Rectangle> componentBoxes;
+    private ArrayList<Rectangle> choiceBoxes;
 
     @Override
     public void create() { //imports all the assets
         //81 complex items and 65 basic items
-        //basic items are 0-64 and complex items are 65-145
+        //basic items are 0-64 and complex items are 65-146
         batch = new SpriteBatch();//contains all sprites to be drawn to the screen
-        itemTextures = new Texture[146];
-        itemSprites = new Sprite[146];
+        itemTextures = new Texture[147];
+        itemSprites = new Sprite[147];
+        shapeRenderer = new ShapeRenderer();
 
         //readFilesWindows();
         readFilesOSX();
@@ -48,18 +64,34 @@ public class ShopGame extends ApplicationAdapter {
         consecutive = 0;
         consecutiveText = new BitmapFont();
 
-        String itemList = itemListHandle.readString();
-        String[] itemString = itemList.split("\n");
-        itemsDict = new HashMap<String, Integer>();
-        for(int i=0;i<itemString.length;i++){
-            itemsDict.put(itemString[i],i);
+        //puts items in the item dict with icons
+        String itemsString = itemListHandle.readString();
+        String[] itemStrings = itemsString.split("\n");
+        itemsDict = new HashMap<String, Item>();
+        for(int i = 0; i < itemStrings.length; i++) {
+           itemsDict.put(itemStrings[i], new Item(itemStrings[i], itemSprites[i]));
         }
 
-        recipes = new ArrayList<Integer>();
+        //gives all items their build components
         String recipesString = itemAssemblyHandle.readString();
-        String[] recipeString = recipesString.split("\n");
-        for(int i=0;i<recipeString.length;i++){
-            //make an array list of the recipes using the numerical value of the items
+        String[] recipeStrings = recipesString.split("\n");
+        for(String recipeString: recipeStrings) {
+            String item = recipeString.substring(0, recipeString.indexOf(":"));
+            String[] componentStrings = recipeString.substring(recipeString.indexOf(":")+1).split(",");
+            ArrayList<Item> components = new ArrayList<Item>();
+            for (String componentString: componentStrings) {
+                components.add(itemsDict.get(componentString));
+            }
+            itemsDict.get(item).setComponents(components);
+        }
+
+        //gives all items their descriptions
+        String descriptionsString = itemDescriptionHandle.readString();
+        String[] descriptionStrings = descriptionsString.split("///\n");
+        for (String descriptionString: descriptionStrings) {
+            String item = descriptionString.substring(0, descriptionString.indexOf("\n"));
+            String description = descriptionString.substring(descriptionString.indexOf("\n")+1);
+            itemsDict.get(item).setDescription(description);
         }
     }
 
@@ -84,7 +116,8 @@ public class ShopGame extends ApplicationAdapter {
             itemSprites[index] = new Sprite(itemTextures[index]);
         }
         itemListHandle = Gdx.files.internal("Spreadsheets\\ItemList.txt");
-        itemAssemblyHandle = Gdx.files.internal("Spreadsheets\\ItemDescription.txt");
+        itemAssemblyHandle = Gdx.files.internal("Spreadsheets\\ItemAssembly.txt");
+        itemDescriptionHandle = Gdx.files.internal("Spreadsheets\\ItemDescription.txt");
     }
 
     public void readFilesOSX() {
@@ -108,15 +141,20 @@ public class ShopGame extends ApplicationAdapter {
             itemSprites[index] = new Sprite(itemTextures[index]);
         }
         itemListHandle = Gdx.files.internal("core/assets/Spreadsheets/ItemList.txt");
-        itemAssemblyHandle = Gdx.files.internal("core/assets/Spreadsheets/ItemDescription.txt");
+        itemAssemblyHandle = Gdx.files.internal("core/assets/Spreadsheets/ItemAssembly.txt");
+        itemDescriptionHandle = Gdx.files.internal("core/assets/Spreadsheets/ItemDescription.txt");
     }
 
     @Override
     public void dispose() { //disposes of all the assets
-        for(Texture itemTexture: itemTextures){
+        int i = 1;
+        for(Texture itemTexture: itemTextures) {
+            System.out.println(i);
             itemTexture.dispose();
+            i++;
         }
         batch.dispose();
+        shapeRenderer.dispose();
         guessesLeftText.dispose();
         scoreText.dispose();
         consecutiveText.dispose();
@@ -129,18 +167,20 @@ public class ShopGame extends ApplicationAdapter {
         getInput();
         update();
 
-        /**
-         * Drawing
-         */
         Gdx.gl.glClearColor(0,0,0,1); //clears the background and set it black
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        //draw stuff here
+        batch.begin();//draw stuff after here
         guessesLeftText.draw(batch, "Guesses Left: " + guessesLeft, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 - 10);
         scoreText.draw(batch, "Score: " + score, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 - 30);
         consecutiveText.draw(batch, consecutive +" in a row", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 - 50);
-        itemSprites[0].draw(batch);
+        Item item = itemsDict.get("Blink Dagger");
+        item.getIcon().draw(batch);
         batch.end();
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        //shapeRenderer.rect();
+        shapeRenderer.end();
     }
 
     /**
@@ -148,10 +188,10 @@ public class ShopGame extends ApplicationAdapter {
      */
     public void getInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            itemSprites[0].setPosition(itemSprites[0].getX() + 5, itemSprites[0].getY());
+            itemsDict.get("Blink Dagger").getIcon().setPosition(itemsDict.get("Blink Dagger").getIcon().getX() + 5, itemsDict.get("Blink Dagger").getIcon().getY());
         }
         if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            itemSprites[0].setPosition(0, 0);
+            itemsDict.get("Blink Dagger").getIcon().setPosition(0, 0);
             guessesLeft--;
         }
     }
